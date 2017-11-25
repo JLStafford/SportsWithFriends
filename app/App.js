706ba269cf812beb7b9869bd1eecc8da9be31265
jlstafford <jlowellstafford@gmail.com>
2017-11-25 16:46:55 -0700

    Project setup successful, fixed basic folder structure, and started designing Authentication scene.

---
+++ app/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import Authentication from './scenes/Authentication';
+
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <Authentication />
+    );
+  }
+}
+
